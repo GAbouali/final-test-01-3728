b7195bd09563a25f3e61d00131d3533265ccd748
Your Gomaa <gomaamhmd@gmail.com>
2023-02-25 22:37:21 +0300

    upd1

--- FinalTest_01/Program.cs
+++ FinalTest_01/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World 01!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+// меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] ->
+//---------------------------------------------------------------------
+
+
+
+//                                " Code Driver "
+Console.WriteLine("enter the text: ");             // print the text from user.
+string? phrase = Console.ReadLine() ?? "0";       //read the inputs as a string.
+
+
+//                                 " methods "
+//method to form the original array.
+string[] CreatOrgArray(string phrase)
+{
+    string[] words = phrase.Split(' ');  // split the text according to spaces.
+    //print the original array.
+    Console.Write("the original array : [\"");
+    for (int i = 0; i < words.Length - 1; i++)
+    {
+        Console.Write(words[i] + "\",\"");
+    }
+    Console.WriteLine(words[words.Length - 1] + "\"]");
+    // return the original array.
+    return words;
+}
+
